615a3c10c30ed0cc8805035ecdb75e9813dedb3e
Kimthean <theantian089@gmail.com>
2024-11-04 18:02:53 +0700

    idk what is this

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,23 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      -- Add more file types and formatters as needed
+    },
+  },
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        eslint = {
+          settings = {
+            workingDirectories = { mode = "auto" },
+            experimental = {
+              useFlatConfig = true,
+            },
+          },
+        },
+      },
+    },
+  },
+}
